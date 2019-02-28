4ec54d3333caaa05080319486d6de8f09497c553
liujiuchao <1837234803@qq.com>
2019-02-28 19:03:45 +0800

    修改登录注册页样式，并修复验证码不能发送bug

--- pages/sign_up/index.vue
+++ pages/sign_up/index.vue
@@ -6,16 +6,17 @@
       </div>
       <h3>注&nbsp;&nbsp;&nbsp;册</h3>
       <div class="inputBox">
-        <input id="phoneNumber" type="text" autocomplete="off" @focus="phoneNumOnfocus()" @blur="phoneNumOnblur()" value="手机号">
-        <p class="inputHint" v-if="!phoneNumStatus">请输入正确的手机号</p>
-        <input autocomplete="off" id="username" type="text" @focus="usernameOnfocus()" @blur="usernameOnblur()" value="昵称">
-        <p class="inputHint" v-if="!usernameStatus">请输入正确的昵称</p>
+        <input autocomplete="off" id="username" type="text" @focus="usernameOnfocus()" @blur="usernameOnblur()" value="用户名">
+        <p class="inputHint" v-if="!usernameStatus">用户名由3到15个字符组成</p>
         <p id="passwordTxt">密码</p>
         <input autocomplete="off" id="password" type="password" @focus="passwordOnfocus()" @blur="passwordOnblur()">
         <p class="inputHint" v-if="!passwordStatus">密码应为6-18位字母或数字</p>
         <p id="passwordAgain">确认密码</p>
         <input autocomplete="off" id="again" type="password" @focus="againOnfocus()" @blur="againOnblur()">
         <p class="inputHint">{{againStatus===-1?"请先正确输入密码":againStatus===1?"两次输入不一致":""}}</p>
+        <input id="phoneNumber" type="text" autocomplete="off" @focus="phoneNumOnfocus()" @blur="phoneNumOnblur()" value="手机号">
+        <p class="inputHint" v-if="!phoneNumStatus">请输入正确的手机号</p>
+        <div></div>
         <input id="authCode" type="text" autocomplete="off" @focus="authCodeOnfocus()" @blur="authCodeOnblur()" value="请输入验证码">
         <el-button id="sendCode" @click="clickSendCode()" type="button">获取验证码</el-button>
         <p class="inputHint" v-if="!authCodeStatus">请输入正确的验证码</p>
@@ -41,14 +42,14 @@
 </template>
 
 <script>
+  import axios from "axios"
   export default {
     data(){
       return {
         //-1为初始状态或点击状态，0为输入有误或为空，1为输入正确
-        //验证码格式为六位数字
+        usernameStatus:-1,
         phoneNumStatus:-1,
         authCodeStatus:-1,
-        usernameStatus:-1,
         passwordStatus:-1,
         //-1为密码未正确输入，0为初始或输入框为空，1为密码和再次输入密码不一致，2为密码和再次输入密码一致
         againStatus:0,
@@ -56,6 +57,26 @@
       }
     },
     methods:{
+      usernameOnfocus(){
+        const username = document.getElementById('username');
+        this.usernameStatus = -1;
+        if(username.value=='用户名'){
+          username.value=''
+        }
+      },
+      usernameOnblur(){
+        const username = document.getElementById('username').value;
+        if(username.length>15 || username.length<3){
+          console.log(username.length+" ，不在范围");
+          this.usernameStatus = 0;
+          if(username==''){
+            document.getElementById('username').value='用户名'
+          }
+        }else {
+          console.log(username.length+" ，在范围");
+          this.usernameStatus = 1;
+        }
+      },
       phoneNumOnfocus(){
         const phoneNum = document.getElementById('phoneNumber');
         this.phoneNumStatus = -1;
@@ -91,18 +112,6 @@
         if(authCodePattern.test(authCode.value)){
           this.authCodeStatus = 1;
         }
-      },
-      usernameOnfocus(){
-        const username = document.getElementById('username');
-        this.usernameStatus = -1;
-        if(username.value=='昵称'){
-          username.value=''
-        }
-      },
-      usernameOnblur(){
-        const username = document.getElementById('username');
-        this.usernameStatus = 0;
-        username.value==''?username.value='昵称':this.usernameStatus = 1;
       },
       passwordOnfocus() {
         this.passwordStatus = -1;
@@ -145,7 +154,7 @@
             username = document.getElementById('username').value,
             password = document.getElementById('password').value,
             addDTO = {"age":0,"status":1,"password":password,"phoneNumber":phoneNum,"username":username};
-          axios.post('http://47.99.155.46:8081/account/validate'+"?code="+authCode,addDTO)
+          axios.post('login/account/validate'+"?code="+authCode,addDTO)
             .then(response =>{
               console.log(response);
               if(response.data.status === 1){
@@ -165,13 +174,13 @@
       },
       clickSendCode(){
         console.log(this.clickTimes++)
-        let wait = 5;
+        let wait = 60;
         const sendCode = document.getElementById("sendCode");
         if(this.phoneNumStatus === 1){
           time(sendCode,wait);
           const phoneNum = document.getElementById('phoneNumber').value,
             phoneNumber = "phoneNumber=" + phoneNum;
-          axios.post('/login/account/send',phoneNumber)
+          axios.post('login/account/send',phoneNumber)
             .then(response => {
               console.log(response);
               if(response.data.status===1){
@@ -195,7 +204,7 @@ function time(dom,wait) {
   if (wait === 0) {
     dom.removeAttribute("disabled");
     dom.innerHTML = "获取验证码";
-    wait = 5;
+    wait = 60;
   } else {
     dom.setAttribute("disabled", true);
     dom.innerHTML = wait + "秒后重试";
