43b0ade9d2bce14363fa88a9bfd8f3aa85457862
codkui1 <410232436@qq.com>
2019-03-01 10:42:46 +0800

    修改地址为线上地址

--- nuxt.config.js
+++ nuxt.config.js
@@ -55,8 +55,10 @@ module.exports = {
   },
 
   proxy: {
-    '/api/': { target: 'http://localhost:8080'},
-    '/login/': { target: 'http://localhost:8081'}
+    // '/api/': { target: 'http://localhost:8080'},
+    // '/login/': { target: 'http://localhost:8081'}
+    '/api/': { target: 'http://amovauto.com:8080'},
+    '/login/': { target: 'http://amovauto.co:8081'}
   },
   /*
   ** Build configuration
