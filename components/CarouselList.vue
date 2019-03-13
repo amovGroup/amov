26c97fbe84b83a45d09e7b34d46caf74568f01cb
liujiuchao <1837234803@qq.com>
2019-03-13 16:35:22 +0800

    删除轮播图跳转功能

--- components/CarouselList.vue
+++ components/CarouselList.vue
@@ -2,7 +2,7 @@
     <div class="sowing-map">
     <el-carousel :interval="5000" :initial-index="index"	arrow="always" class="banner">
         <el-carousel-item v-for="item in pics" :key="item.carouselId">
-          <a @click="printItem(item.carouselId)" style="cursor: pointer"><img v-bind:src="item.imageUrl" :alt="item.title" :title="item.title" class="carousel"></a>
+          <img v-bind:src="item.imageUrl" :alt="item.title" :title="item.title" class="carousel">
         </el-carousel-item>
     </el-carousel>
     </div>
@@ -14,18 +14,6 @@ export default {
   async asyncData({ $axios }) {
     const json = await $axios.$post('/api/carousel/list', {location: 0})
     return { pics: json.body }
-  },
-  methods:{
-    printItem(carouselId){
-      if (process.browser) {
-        switch(carouselId){
-          case 1:window.location.href = '/';break;
-          case 2:window.location.href = '/product';break;
-          case 3:window.location.href = '/news';break;
-          case 4:window.location.href = '/software';break;
-        }
-      }
-    }
   }
 }
 </script>
